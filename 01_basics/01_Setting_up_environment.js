0ba0989de62eeffbf969c52bfaa05b5284d8a5c0
Swapnil <deshmukhswapnil316@gmail.com>
2024-01-01 16:27:26 +0530

    Adding projects

--- 01_basics/01_Setting_up_environment.js
+++ 01_basics/01_Setting_up_environment.js
@@ -1,1 +1,13 @@
-console.log("Hi");
+// console.log("Hi");
+
+// We can use Node to run the js 
+// That is node filename.js
+
+// 1) Let, const and var ->
+
+/**
+ * 1. const --> const keyword storing
+ * 2. var --> for storing variable keyword, but it has mainly problem in scope
+ * 3. let --> used to store the variable keyword, and it can have different scope different values.
+ *      i.e. block scope and functional scope
+ */
